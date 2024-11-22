3e2547e441131b2ff0f974d6c5246fdf6ae50bb2
Victor Hugo <vrolim@ipdanalytics.com>
2024-11-22 16:58:05 -0300

    Calendar app

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import CalendarApp from "./Components/CalendarApp"
+import './Components/CalendarApp.css'
+
+const App = () => {
+  return <div className="container"><CalendarApp/></div>
+}
+
+export default App
